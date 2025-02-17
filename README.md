13acc8afa3219427db5289f3b43440326fa0793e
Brandon Arturo Gomez Montenegro <developer@vultuscompany.com>
2025-02-17 10:04:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# challenge_banpay
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/challenge_banpay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.challenge_banpay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
